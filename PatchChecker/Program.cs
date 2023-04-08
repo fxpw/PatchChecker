a6e5affa78440b321f937bf04d9e07e63d18fb4f
fxpw <fxpw@outlook.com>
2023-04-08 22:35:09 +0300

    comments

--- PatchChecker/Program.cs
+++ PatchChecker/Program.cs
@@ -21,81 +21,66 @@ public static class HttpClientUtils
         }
     }
 }
-public struct Haash
-{
-    public string hash { get; set; }
-
-    //public  int Count { get; set; }
-}
 
 public class Program
 {
-    // Uncomment the following line to resolve.
+    public static string pathToWow = "D:\\games\\sirus\\World of Warcraft Sirus";
+
     static HttpClient httpClient = new HttpClient();
 
+    #region Check Patch md5
     [DllImport("PatchChecker.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
     public static extern bool CheckPatch(string pathToFile, string md5);
-
-    public delegate void ResponseDelegate(string s);
-    [DllImport("PatchChecker.dll", EntryPoint = "GetMD5", CallingConvention = CallingConvention.StdCall)]
+    #endregion
+    #region get md5
+    //public delegate void ResponseDelegate(string s);
+    //[DllImport("PatchChecker.dll", EntryPoint = "GetMD5", CallingConvention = CallingConvention.StdCall)]
     /*
      *  GetMD5(pathToWow, pathToFileFromWowDir, s =>
         {
             Console.WriteLine(s);
         });
      */
-    public static extern void GetMD5(string path, string name, ResponseDelegate response);
+    //public static extern void GetMD5(string path, string name, ResponseDelegate response);
+    #endregion
 
-    public static string pathToWow = "D:\\games\\sirus\\World of Warcraft Sirus";
     static async Task Main(){
         await Task.Run(async () =>
         {
-            // определяем данные запроса
+            // запросики
             using HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "http://51.15.228.31:8080/api/client/patches");
-
-            // получаем ответ
             using HttpResponseMessage response = await httpClient.SendAsync(request);
-
             string content = await response.Content.ReadAsStringAsync();
-            Console.WriteLine(pathToWow);
+            //Console.WriteLine(pathToWow);
             Newtonsoft.Json.Linq.JObject obj = Newtonsoft.Json.Linq.JObject.Parse(content);
+            // проверка патчей
             for (int i = 1; i < obj?["patches"]?.Count(); i++){
-                
                 var info = obj?["patches"]?[i];
-                Console.WriteLine(info);
-                //Console.WriteLine(obj?["patches"]?[i]?["filename"]);
-                //Console.WriteLine(obj?["patches"]?[i]?["path"]);
+                //Console.WriteLine(info);
                 var time  = DateTime.Parse((string)info?["updated_at"], DateTimeFormatInfo.InvariantInfo);
                 var timeNow = DateTime.Now;
                 var pathToFile = pathToWow + (string)info?["path"] + (string)info?["filename"];
                 var urlForDownload = (string)info?["host"] + (string)info?["storage_path"];
-                ////if (time > timeNow)
+                //if (time > timeLastCheck) //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!todo!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! проверка по дате получается если прикрутить то можно скипать лишние проверки
                 //{ 
                 //Console.WriteLine(time);
                 var isNeedUpdate = CheckPatch(pathToWow + info?["path"] + info?["filename"], (string)info?["md5"]);
-                if (isNeedUpdate)
-                {
-                    using (HttpClient client = new System.Net.Http.HttpClient())
-                    {
+                if (isNeedUpdate){
+                    //using (HttpClient client = new System.Net.Http.HttpClient())
+                    //{
                         
                         //if ((string)info["filename"] == "patch-ruRU-k.mpq")
                         //{
-                            if (File.Exists(Path.Combine(pathToFile)))
-                            {
+                            if (File.Exists(pathToFile)){
                                 // If file found, delete it    
-                                File.Delete(Path.Combine(pathToFile));
+                                File.Delete(pathToFile);
                                 //Console.WriteLine(pathToFile);
-
                             }
                             var uri = new Uri(urlForDownload);
-                            await client.DownloadFileTaskAsync(uri, pathToFile);
+                            await httpClient.DownloadFileTaskAsync(uri, pathToFile);
                             //return;
                         //}
-                       
-                        //var uri = new Uri(urlForDownload);
-                        //await client.DownloadFileTaskAsync(uri, pathToFile);
-
-                    }
+                    //}
                 }
                 //return;
             }
